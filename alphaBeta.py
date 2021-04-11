0a498293773c348eaac15d46fdbed49ef0fd2268
Azoies1 <thomasle98@outlook.com>
2021-04-10 22:39:11 -0400

    Added different files with TODO in each

---
+++ alphaBeta.py
@@ -0,0 +1,26 @@
+import Node
+
+#TODO implement minimax, alpha-beta pruning algorithm
+# record values for the print out
+def miniMax(node, depth, alpha, beta, maxPlayer):
+    return 0
+
+
+
+
+#TODO print out the result of the alphabeta algo
+# The best move (i.e., the tokens number that is to be taken) for the current player (as computed by your
+# alpha-beta algorithm)
+# The value associated with the move (as computed by your alpha-beta algorithm)
+# The total number of nodes visited
+# The number of nodes evaluated (either an end game state or the specified depth is reached)
+# The maximum depth reached (the root is at depth 0)
+# The average effective branching factor (i.e., the average number of successors that are not pruned)
+
+def printOutput():
+    print("Move: ", "andre?")
+    print("Value: ", "andre?")
+    print("Number of Nodes Visited: ", "andre?")
+    print("Number of Nodes Evaluated: ", "andre?")
+    print("Max Depth Reached: ", "andre?")
+    print("Avg Effective Branching Factor: ", "andre?")
