83f279a0e771bc857fad386ddf7990b9a3ebe154
Azoies1 <thomasle98@outlook.com>
2021-04-20 00:44:39 -0400

    Missing win scenarios

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import playTheGame
 import alphaBeta
 from node import *
 import copy
+import re
+
 
 class Statistic:
 
@@ -13,33 +15,46 @@ class Statistic:
         self.nodesEvaluated = 0
         self.deepest = math.inf
 
+def print(node,stats,value):
+    print("Move: ", node.move)
+    print("Value: ", value)
+    print("Number of Nodes Visited: ", stats.nodesVisited)
+    print("Number of Nodes Evaluated: ", stats.nodesEvaluated)
+    print("Max Depth Reached: ", stats.deepest)
+    print("Avg Effective Branching Factor: ", "andre?")
+
 if __name__ == "__main__":
-    gameOrAlgo = input("Would you like to play the game or run the algo?")
-    if "game" in gameOrAlgo.lower():
-        l = [2,3,4,5,6,7]
-        N = node(l, 4, -1)
-        print(N.getEvalNumber("maxplayer"))
-    elif "algo" in gameOrAlgo.lower():
-        # TODO get user inputs.
-        #  PNT Player <#tokens> <#taken_tokens> <list_of_taken_tokens> <depth>
-        #  this can be done either by asking the user or by taking in a file.
-        #  can also just be hard coded... in format like [7,2,3,6,0]
+    gameInput = input("Would you like to play the game or run the algo?")
+    gameInput = [int(i) for i in gameInput.split() if i.isdigit()]
+    tokens = int(gameInput[0])
+    taken_tokens = int(gameInput[1])
+    list_of_taken_tokens = []
+    if taken_tokens > 0:
+        list_of_taken_tokens = [int(i) for i in gameInput[2:taken_tokens+2]]
+    depth = gameInput[len(gameInput) - 1]
 
-        # TODO run the alphabeta algo
-        #  in the alphabeta algo, the prints for the outputs will be done
-        fullList = [1,2,3,4,5]
-        cutList = [1,2]
-        values = []
-        stats = Statistic()
-        for num in cutList:
-            tempList = copy.deepcopy(fullList)
-            tempList.remove(num)
-            alpha = -math.inf
-            beta = math.inf
-            values.append(alphaBeta.miniMax(Node(tempList, num,""), 10, alpha, beta, "maxplayer",stats))
-        
-        for value in values:
-            print(value)
+    if depth == 0:
+        depth = math.inf
 
+    fullList = list(range(1, tokens+1))
+
+    cutList = None
+    if taken_tokens == 0:
+        initalNode = Node(fullList,0,"")
+        cutList = alphaBeta.possibleMoves(initalNode)
     else:
-        exit()
+        for num in list_of_taken_tokens:
+            fullList.remove(num)
+        initalNode = Node(fullList, list_of_taken_tokens[len(list_of_taken_tokens)-1], "")
+        cutList = alphaBeta.possibleMoves(initalNode)
+
+    values = []
+    stats = Statistic()
+    for num in cutList:
+        tempList = copy.deepcopy(fullList)
+        tempList.remove(num)
+        alpha = -math.inf
+        beta = math.inf
+        values.append(alphaBeta.miniMax(Node(tempList, num, ""), depth, alpha, beta, "maxplayer", stats))
+    for i in values:
+        print(i)
