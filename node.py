4485c0f00f285c8ec5af6061a65c339490a76b1b
Michael Arabian <michael-nareg99@hotmail.com>
2021-04-20 13:04:35 -0400

    gameOver

--- node.py
+++ node.py
@@ -1,3 +1,5 @@
+import copy
+
 class Node:
 
     def __init__(self,listOfTokens, move, lastMoves):
@@ -20,15 +22,15 @@ class Node:
 
 
     # Returns number of succesors
-    def getSuccesorCount(self, move):
+    def getSuccesorCount(self, move, listok):
         count = 0
-        for i in range(len(self.listOfTokens)):
-            if move > self.listOfTokens[i]:
-                if (move % self.listOfTokens[i] == 0):
+        for i in range(len(listok)):
+            if move > listok[i]:
+                if (move % listok[i] == 0):
                     count += 1
 
-            if move < self.listOfTokens[i]:
-                if (self.listOfTokens[i] % move == 0):
+            if move < listok[i]:
+                if (listok[i] % move == 0):
                     count += 1
 
         return count+1
@@ -45,9 +47,25 @@ class Node:
 
         return largestPrime
 
+    def isGameOver(self, move):
+        tempList = copy.deepcopy(self.listOfTokens)
+        tempList.remove(move)
+        # If count is 1, game is over
+        if(getSuccesorCount(move, tempList) == 1):
+            return True
+        
+        return False
+
+
+
+
     #TODO add heuristic for return of eval of the node
     def getEvalNumber(self, player):
         if player == "maxPlayer":
+
+            if isGameOver(self.move, self.listOfTokens):
+                return -1
+
             # TOKEN 1 NOT TAKEN
             if 1 in self.listOfTokens:
                 return 0
@@ -62,7 +80,7 @@ class Node:
 
             # LAST MOVE WAS PRIME
             if (self.isPrime(self.move)):
-                count = self.getSuccesorCount(self.move)
+                count = self.getSuccesorCount(self.move,self.listOfTokens)
                 if (count % 2 == 0):
                     return -0.7
                 else:
@@ -71,7 +89,7 @@ class Node:
             # LAST MOVE WAS COMPOSITE
             if (not (self.isPrime(self.move))):
                 largestPrime = self.getLargestPrime(self.move)
-                count = self.getSuccesorCount(largestPrime)
+                count = self.getSuccesorCount(largestPrime,self.listOfTokens)
                 if (count % 2 == 0):
                     return -0.6
                 else:
@@ -79,6 +97,10 @@ class Node:
 
         else:
             # TOKEN 1 NOT TAKEN
+
+            if isGameOver(self.move, self.listOfTokens):
+                return 1
+
             if 1 in self.listOfTokens:
                 return 0
 
@@ -92,7 +114,7 @@ class Node:
 
             # LAST MOVE WAS PRIME
             if (self.isPrime(self.move)):
-                count = self.getSuccesorCount(self.move)
+                count = self.getSuccesorCount(self.move,self.listOfTokens)
                 if (count % 2 == 0):
                     return 0.7
                 else:
@@ -101,7 +123,7 @@ class Node:
             # LAST MOVE WAS COMPOSITE
             if (not (self.isPrime(self.move))):
                 largestPrime = self.getLargestPrime(self.move)
-                count = self.getSuccesorCount(largestPrime)
+                count = self.getSuccesorCount(largestPrime,self.listOfTokens)
                 if (count % 2 == 0):
                     return 0.6
                 else:
